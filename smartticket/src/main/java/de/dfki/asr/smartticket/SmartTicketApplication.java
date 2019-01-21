2d2d8a69042851b660040ba3029ae797dd690433
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2019-01-21 12:39:06 +0100

    springboot application base class

---
+++ smartticket/src/main/java/de/dfki/asr/smartticket/SmartTicketApplication.java
@@ -0,0 +1,11 @@
+package de.dfki.asr.smartticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTicketApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTicketApplication.class, args);
+    }
+}
