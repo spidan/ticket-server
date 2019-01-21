3281184066fb7031c6a8050d9dec53b1a34bdb06
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2019-01-21 16:14:21 +0100

    Simple test service for checking whether the service is alive

---
+++ smartticket/src/main/java/de/dfki/asr/smartticket/service/Echo.java
@@ -0,0 +1,16 @@
+package de.dfki.asr.smartticket.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Echo {
+
+	@GetMapping("/echo")
+	@ResponseBody
+	public String echoInput(@RequestParam(name = "message", required = false, defaultValue = "echoService") String message) {
+		return message;
+	}
+}
