019de27e35a13a6da078a4ebcbdb8246578b5810
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2019-01-21 16:15:32 +0100

    Added a class to wrap an incoming post request to a request for the barti service
    This will later happen in the adapter

---
+++ smartticket/src/main/java/de/dfki/asr/smartticket/ticket/Request.java
@@ -0,0 +1,33 @@
+package de.dfki.asr.smartticket.ticket;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class Request {
+	
+	private final String TICKET_URL = "http://localhost:8080/ticket";
+	private final TicketConfiguration configuration;
+	
+	public Request(TicketConfiguration configuration) {
+		this.configuration = configuration;
+	}
+	
+	public HttpResponse send() throws IOException {
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpPost postRequest = new HttpPost(TICKET_URL);
+		ObjectMapper mapper = new ObjectMapper();
+		String configAsString = mapper.writeValueAsString(configuration);
+		HttpEntity entity = new StringEntity(configAsString, ContentType.APPLICATION_JSON);
+		postRequest.addHeader("Content-Type", "application/json");
+		postRequest.addHeader("Authorization-Key", "46fd1c14-a985-4053-bc22-708f45b7d971");
+		postRequest.setEntity(entity);
+		return client.execute(postRequest);
+	}
+}
