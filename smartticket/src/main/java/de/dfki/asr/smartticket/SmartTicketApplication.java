e652a627d994fc92fa4249456ab6cc63266110e2
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2019-01-24 14:27:39 +0100

    Suppress helper class warning on the spring boot application main class

--- smartticket/src/main/java/de/dfki/asr/smartticket/SmartTicketApplication.java
+++ smartticket/src/main/java/de/dfki/asr/smartticket/SmartTicketApplication.java
@@ -3,6 +3,7 @@ package de.dfki.asr.smartticket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
 public class SmartTicketApplication {
 	public static void main(final String[] args) {
