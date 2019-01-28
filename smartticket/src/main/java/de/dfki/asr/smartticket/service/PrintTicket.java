281c9a966009b3adf534db208448b59db1adc370
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2019-01-28 15:49:02 +0100

    Moved exception handlers for spring boot in external utility class

--- smartticket/src/main/java/de/dfki/asr/smartticket/service/PrintTicket.java
+++ smartticket/src/main/java/de/dfki/asr/smartticket/service/PrintTicket.java
@@ -1,19 +1,18 @@
 package de.dfki.asr.smartticket.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import de.dfki.asr.smartticket.Rest.ExceptionHandlers.SpringExceptionHandlers;
 import de.dfki.asr.smartticket.ticket.Request;
 import de.dfki.asr.smartticket.ticket.TicketConfiguration;
 import java.io.IOException;
 import org.apache.http.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class PrintTicket {
@@ -29,21 +28,11 @@ public class PrintTicket {
 			HttpResponse response = ticketRequest.send();
 			return response.toString();
 		} catch (JsonProcessingException ex) {
-			return handleJsonException(ex);
+			logger.error("Invalid JSON in input: " + ex.getMessage());
+			return SpringExceptionHandlers.handleJsonException(ex);
 		} catch (IOException ex) {
-			return handleIoException(ex);
+			logger.error("Error in connection: " + ex.getMessage());
+			return SpringExceptionHandlers.handleIoException(ex);
 		}
-	}
-
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	private String handleIoException(final IOException ex) {
-		logger.error("Error in connection: " + ex.getMessage());
-		return ex.getMessage();
-	}
-
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	private String handleJsonException(final JsonProcessingException ex) {
-		logger.error("Invalid JSON in input: " + ex.getMessage());
-		return ex.getMessage();
 	}
 }
