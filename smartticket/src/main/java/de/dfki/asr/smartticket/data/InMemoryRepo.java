1ec8276dd5e981360b30eaf7be2d2abdc8051367
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2019-01-28 15:49:27 +0100

    Simple stub for using in memory repos (only supports very basic write so far)

---
+++ smartticket/src/main/java/de/dfki/asr/smartticket/data/InMemoryRepo.java
@@ -0,0 +1,27 @@
+package de.dfki.asr.smartticket.data;
+
+import org.eclipse.rdf4j.model.impl.SimpleStatement;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.RepositoryException;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.sail.memory.MemoryStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InMemoryRepo {
+	private static final Logger LOG = LoggerFactory.getLogger(InMemoryRepo.class);
+	private final Repository repo;
+
+	public InMemoryRepo() {
+		repo = new SailRepository(new MemoryStore());
+	}
+
+	public void write(final Iterable<SimpleStatement> statements) {
+		try (RepositoryConnection con = repo.getConnection()) {
+			con.add(statements);
+		} catch (RepositoryException ex) {
+			LOG.error("Failed to access triplestore: " + ex.getMessage());
+		}
+	}
+}
