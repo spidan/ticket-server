1c9c4ea3937188f5fbbabb5bbdad9fbe58d3df2e
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2019-01-30 12:20:58 +0100

    Added a configuration for the SpringApplication to add custom converters

---
+++ smartticket/src/main/java/de/dfki/asr/smartticket/AppConfig.java
@@ -0,0 +1,17 @@
+package de.dfki.asr.smartticket;
+
+import de.dfki.asr.smartticket.Rest.RDFMessageConverter;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public HttpMessageConverters customConverters() {
+		HttpMessageConverter rdfConverter = new RDFMessageConverter<>();
+		return new HttpMessageConverters(rdfConverter);
+	}
+}
