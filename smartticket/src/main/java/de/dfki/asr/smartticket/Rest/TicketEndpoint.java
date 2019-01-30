3669f5ed947a5dc8a904a9e9a82cca06f1d60597
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2019-01-30 12:21:50 +0100

    Removed from the TicketEndpoint what is handled now directly by the message converter

--- smartticket/src/main/java/de/dfki/asr/smartticket/Rest/TicketEndpoint.java
+++ smartticket/src/main/java/de/dfki/asr/smartticket/Rest/TicketEndpoint.java
@@ -1,13 +1,8 @@
 package de.dfki.asr.smartticket.Rest;
 
 import de.dfki.asr.smartticket.Rest.ExceptionHandlers.SpringExceptionHandlers;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import org.eclipse.rdf4j.model.Model;
-import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.RDFParseException;
-import org.eclipse.rdf4j.rio.Rio;
 import org.eclipse.rdf4j.rio.UnsupportedRDFormatException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,21 +15,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TicketEndpoint {
 	private static final Logger LOG = LoggerFactory.getLogger(TicketEndpoint.class);
-	private static final String BASE_URI = "http://www.smartmaas.de";
 
 	@RequestMapping(value = "/ticket",
 			method = RequestMethod.POST,
 			consumes = "text/turtle")
 	@ResponseBody
-	public String receiveTicket(@RequestBody final String model) {
+	public String receiveTicket(@RequestBody final Model model) {
 		try {
-			InputStream modelStream = new ByteArrayInputStream(model.getBytes("UTF-8"));
-			Model inputModel = Rio.parse(modelStream, BASE_URI, RDFFormat.TURTLE);
-			LOG.debug(inputModel.toString());
-			return inputModel.toString();
-		} catch (IOException ex) {
-			LOG.error(ex.getMessage());
-			return SpringExceptionHandlers.handleIoException(ex);
+			LOG.debug(model.toString());
+			return model.toString();
 		} catch (RDFParseException ex) {
 			LOG.error("Error parsing RDF: " + ex.getMessage());
 			return SpringExceptionHandlers.handleRDFParseException(ex);
