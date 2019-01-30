2c2e417e5a649dcd64b1da304f789554e9777a42
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2019-01-30 12:21:27 +0100

    Very basic placeholder implementation of the RDFMessageConverter class

--- smartticket/src/main/java/de/dfki/asr/smartticket/Rest/RDFMessageConverter.java
+++ smartticket/src/main/java/de/dfki/asr/smartticket/Rest/RDFMessageConverter.java
@@ -1,26 +1,38 @@
 package de.dfki.asr.smartticket.Rest;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.Rio;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.AbstractHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 
 public class RDFMessageConverter<T> extends AbstractHttpMessageConverter<T> {
+	private static final String BASE_URI = "http://www.smartmaas.de";
 
-	@Override
-	protected boolean supports(Class<?> clazz) {
-		throw new UnsupportedOperationException("Not supported yet.");
+	public RDFMessageConverter() {
+		super(new MediaType("text", "turtle", Charset.forName("UTF-8")));
 	}
 
 	@Override
-	protected T readInternal(Class<? extends T> clazz, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
-		throw new UnsupportedOperationException("Not supported yet.");
+	protected boolean supports(final Class<?> clazz) {
+		return true;
 	}
 
 	@Override
-	protected void writeInternal(T t, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
-		throw new UnsupportedOperationException("Not supported yet.");
+	protected T readInternal(final Class<? extends T> clazz, final HttpInputMessage inputMessage)
+	throws IOException, HttpMessageNotReadableException {
+		T inputModel = (T) Rio.parse(inputMessage.getBody(), BASE_URI, RDFFormat.TURTLE);
+		return inputModel;
+	}
+
+	@Override
+	protected void writeInternal(final T t, final HttpOutputMessage outputMessage)
+	throws IOException, HttpMessageNotWritableException {
+		outputMessage.getBody().write(t.toString().getBytes("UTF-8"));
 	}
 }
