86829819b905776c6ece171fd15dc80921828c06
Daniel Spieldenner <DanielSpieldenner@gmail.com>
2019-01-31 12:47:13 +0100

    Added Logger to TicketConfiguration

--- smartticket/src/main/java/de/dfki/asr/smartticket/ticket/TicketConfiguration.java
+++ smartticket/src/main/java/de/dfki/asr/smartticket/ticket/TicketConfiguration.java
@@ -1,9 +1,13 @@
 package de.dfki.asr.smartticket.ticket;
 
+import de.dfki.asr.smartticket.data.InMemoryRepo;
 import lombok.Getter;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TicketConfiguration {
+	private static final Logger LOG = LoggerFactory.getLogger(TicketConfiguration.class);
 	@Getter@Setter
 	private String apiToken;
 	@Getter@Setter
