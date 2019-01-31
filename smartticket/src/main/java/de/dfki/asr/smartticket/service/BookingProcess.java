44c7a067b72e41813df0806e194bfa2c9ff6168f
Daniel Spieldenner <DanielSpieldenner@gmail.com>
2019-01-31 12:47:49 +0100

    Made the repo of the booking process accessible from outside

--- smartticket/src/main/java/de/dfki/asr/smartticket/service/BookingProcess.java
+++ smartticket/src/main/java/de/dfki/asr/smartticket/service/BookingProcess.java
@@ -1,10 +1,12 @@
 package de.dfki.asr.smartticket.service;
 
 import de.dfki.asr.smartticket.data.InMemoryRepo;
+import lombok.Getter;
 import org.eclipse.rdf4j.model.Model;
 
 public class BookingProcess {
 
+    @Getter
     private final InMemoryRepo repo;
 
     public BookingProcess() {
