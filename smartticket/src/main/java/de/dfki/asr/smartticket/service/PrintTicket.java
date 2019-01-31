5a2b4539915258ef468b2042802bbf5b9739b226
Daniel Spieldenner <DanielSpieldenner@gmail.com>
2019-01-31 10:48:55 +0100

    Removed SpringBoot endpoint from PrintTicket

--- smartticket/src/main/java/de/dfki/asr/smartticket/service/PrintTicket.java
+++ smartticket/src/main/java/de/dfki/asr/smartticket/service/PrintTicket.java
@@ -8,21 +8,11 @@ import java.io.IOException;
 import org.apache.http.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 public class PrintTicket {
 	private final Logger logger = LoggerFactory.getLogger(PrintTicket.class);
 
-	@RequestMapping(value = "/printTicket",
-			method = RequestMethod.POST,
-			consumes = "application/json")
-	@ResponseBody
-	public String receiveTicket(@RequestBody final TicketConfiguration config) {
+	public String receiveTicket(final TicketConfiguration config) {
 		Request ticketRequest = new Request(config);
 		try {
 			HttpResponse response = ticketRequest.send();
