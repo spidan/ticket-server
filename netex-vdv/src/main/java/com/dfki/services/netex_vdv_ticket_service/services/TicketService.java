631cf61f933626c09ba754284cd4776e61c50157
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2019-05-02 15:24:04 +0200

    Some exception handling and logging in TicketService

--- netex-vdv/src/main/java/com/dfki/services/netex_vdv_ticket_service/services/TicketService.java
+++ netex-vdv/src/main/java/com/dfki/services/netex_vdv_ticket_service/services/TicketService.java
@@ -3,21 +3,34 @@ package com.dfki.services.netex_vdv_ticket_service.services;
 import com.dfki.services.netex_vdv_ticket_service.Utils;
 import com.dfki.services.netex_vdv_ticket_service.models.Ticket;
 import java.io.IOException;
+import javax.xml.bind.JAXBException;
+import org.slf4j.LoggerFactory;
 
 public class TicketService {
 
+	private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(TicketService.class);
+
 	private static final String DFKI_SERVICE_URL = "http://localhost:8800/ticket/in_xml";
 
-	public void postToDFKITicketService(String text) throws IOException {
+	public void postToDFKITicketService(final String text) throws IOException {
 		Utils.sendXMLPostRequest(DFKI_SERVICE_URL, text);
 	}
 
-	public void postToDFKITicketService(Ticket ticket) throws IOException {
-		Utils.sendXMLPostRequest(DFKI_SERVICE_URL, Utils.convertObjectToXML(ticket));
+	public void postToDFKITicketService(final Ticket ticket) throws IOException {
+		try {
+			Utils.sendXMLPostRequest(DFKI_SERVICE_URL, Utils.convertObjectToXML(ticket));
+		} catch (JAXBException ex) {
+			LOG.error("Request to ticket service failed: " + ex.toString());
+		}
 	}
 
-	public Ticket xmlToTicket(String xml) {
-		Ticket ticket = Utils.convertXmlToTicket(xml);
-		return ticket;
+	public Ticket xmlToTicket(final String xml) {
+		try {
+			Ticket ticket = Utils.convertXmlToTicket(xml);
+			return ticket;
+		} catch (JAXBException ex) {
+			LOG.error("Could not create ticket from XML: " + ex.toString());
+		}
+		return null;
 	}
 }
