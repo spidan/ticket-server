3e3b1cf1e806d30df2b9ccf5c9ca18b432495b39
DESKTOP-R3ROKK7\Kazimov <kazimovekber@gmail.com>
2019-05-09 11:47:21 +0200

    Import optimization

--- src/main/java/com/dfki/services/dfki_ticket_service/models/Ticket.java
+++ src/main/java/com/dfki/services/dfki_ticket_service/models/Ticket.java
@@ -3,7 +3,10 @@ package com.dfki.services.dfki_ticket_service.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Map;
 
 @XmlRootElement
