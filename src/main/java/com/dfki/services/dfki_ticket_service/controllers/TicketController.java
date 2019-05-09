590ec5051c0a777272ef68ae36329a618b376e52
DESKTOP-R3ROKK7\Kazimov <kazimovekber@gmail.com>
2019-05-09 11:48:30 +0200

    Import optimization

--- src/main/java/com/dfki/services/dfki_ticket_service/controllers/TicketController.java
+++ src/main/java/com/dfki/services/dfki_ticket_service/controllers/TicketController.java
@@ -2,15 +2,13 @@ package com.dfki.services.dfki_ticket_service.controllers;
 
 import com.dfki.services.dfki_ticket_service.Utils;
 import com.dfki.services.dfki_ticket_service.models.Ticket;
-import com.dfki.services.dfki_ticket_service.repositories.TicketRepo;
 import com.dfki.services.dfki_ticket_service.services.TicketService;
-import org.eclipse.rdf4j.model.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController("TicketController")
