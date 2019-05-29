37660885a08d32704a79c78c0f4487b40379648e
DESKTOP-R3ROKK7\Kazimov <kazimovekber@gmail.com>
2019-05-29 13:39:06 +0200

    Unnecessary comments are removed

--- src/main/java/com/dfki/services/dfki_ticket_service/controllers/TicketController.java
+++ src/main/java/com/dfki/services/dfki_ticket_service/controllers/TicketController.java
@@ -43,37 +43,5 @@ public class TicketController {
         return new ResponseEntity<>(xmlInput, HttpStatus.OK);
 
     }
-//    @GetMapping("get/allTickets")
-//    public List<Ticket> getAllTickets() {
-//        List<Ticket> tickets = (List<Ticket>) ticketRepo.findAll();
-//        return tickets;
-//    }
-//
-//    @GetMapping("get/ticket/{id}")
-//    public Ticket getTicket(@PathVariable String id) {
-//
-//        List<Ticket> tickets = (List<Ticket>) ticketRepo.findAll();
-//        for (Ticket t : tickets) {
-//            if (t.getId().equals(Long.valueOf(id))) {
-//                return t;
-//            }
-//        }
-//        return null;
-//    }
-//
-//    @DeleteMapping("delete/ticket/{id}")
-//    public ResponseEntity<?> deleteTicket(@PathVariable String id) {
-//        try {
-//            Ticket t = getTicket(id);
-//            if (t != null) {
-//                ticketRepo.delete(t);
-//            } else {
-//                return new ResponseEntity<>("No Item with ID:" + id, HttpStatus.ACCEPTED);
-//            }
-//        } catch (Exception e) {
-//            return new ResponseEntity<>("Process Failed", HttpStatus.NOT_ACCEPTABLE);
-//        }
-//        return new ResponseEntity<>("Data Deleted", HttpStatus.ACCEPTED);
-//    }
 
 }
