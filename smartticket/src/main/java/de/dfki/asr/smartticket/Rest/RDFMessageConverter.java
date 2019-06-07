08419fa9ad0b4b278d8c433866ee6e2b360c01fb
DESKTOP-R3ROKK7\Kazimov <kazimovekber@gmail.com>
2019-06-07 11:59:46 +0200

    No changes have been made in "RDFMessageConverter"

--- smartticket/src/main/java/de/dfki/asr/smartticket/Rest/RDFMessageConverter.java
+++ smartticket/src/main/java/de/dfki/asr/smartticket/Rest/RDFMessageConverter.java
@@ -12,27 +12,27 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 
 public class RDFMessageConverter<T> extends AbstractHttpMessageConverter<T> {
-	private static final String BASE_URI = "http://www.smartmaas.de";
+    private static final String BASE_URI = "http://www.smartmaas.de";
 
-	public RDFMessageConverter() {
-		super(new MediaType("text", "turtle", Charset.forName("UTF-8")));
-	}
+    public RDFMessageConverter() {
+        super(new MediaType("text", "turtle", Charset.forName("UTF-8")));
+    }
 
-	@Override
-	protected boolean supports(final Class<?> clazz) {
-		return true;
-	}
+    @Override
+    protected boolean supports(final Class<?> clazz) {
+        return true;
+    }
 
-	@Override
-	protected T readInternal(final Class<? extends T> clazz, final HttpInputMessage inputMessage)
-	throws IOException, HttpMessageNotReadableException {
-		T inputModel = (T) Rio.parse(inputMessage.getBody(), BASE_URI, RDFFormat.TURTLE);
-		return inputModel;
-	}
+    @Override
+    protected T readInternal(final Class<? extends T> clazz, final HttpInputMessage inputMessage)
+            throws IOException, HttpMessageNotReadableException {
+        T inputModel = (T) Rio.parse(inputMessage.getBody(), BASE_URI, RDFFormat.TURTLE);
+        return inputModel;
+    }
 
-	@Override
-	protected void writeInternal(final T t, final HttpOutputMessage outputMessage)
-	throws IOException, HttpMessageNotWritableException {
-		outputMessage.getBody().write(t.toString().getBytes("UTF-8"));
-	}
+    @Override
+    protected void writeInternal(final T t, final HttpOutputMessage outputMessage)
+            throws IOException, HttpMessageNotWritableException {
+        outputMessage.getBody().write(t.toString().getBytes("UTF-8"));
+    }
 }
