6c5cf5028fdaeb4419deb98de919dc6918da42c1
DESKTOP-R3ROKK7\Kazimov <kazimovekber@gmail.com>
2019-06-07 12:01:35 +0200

    "receiveXmlOrJsonTicket" method is added to TicketEndpoint. This method simply accepts Xml/Json and sends it directly to Dfki_ticket_service. And Dfki_ticket_service sends back this data to smartticket service after converting it into RDF.

--- smartticket/src/main/java/de/dfki/asr/smartticket/Rest/TicketEndpoint.java
+++ smartticket/src/main/java/de/dfki/asr/smartticket/Rest/TicketEndpoint.java
@@ -1,28 +1,55 @@
 package de.dfki.asr.smartticket.Rest;
 
+import de.dfki.asr.smartticket.Rest.ExceptionHandlers.SpringExceptionHandlers;
 import de.dfki.asr.smartticket.service.BookingProcess;
 import de.dfki.asr.smartticket.service.TicketWrapper;
+import de.dfki.asr.smartticket.service.Utils;
 import org.eclipse.rdf4j.model.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.io.IOException;
 
 @Controller
 public class TicketEndpoint {
-	private static final Logger LOG = LoggerFactory.getLogger(TicketEndpoint.class);
+    private static final Logger LOG = LoggerFactory.getLogger(TicketEndpoint.class);
 
-	@RequestMapping(value = "/ticket",
-			method = RequestMethod.POST,
-			consumes = "text/turtle")
-	@ResponseBody
-	public String receiveTicket(@RequestBody final Model model) {
-	    BookingProcess booking = new BookingProcess();
-	    booking.writeRequestToRepo(model);
-	    TicketWrapper ticket = new TicketWrapper(booking.getRepo());
-	    return ticket.receiveTicket();
-	}
+    @RequestMapping(value = "/ticket",
+            method = RequestMethod.POST,
+            consumes = "text/turtle")
+    @ResponseBody
+    public String receiveTicket(@RequestBody final Model model) {
+        System.out.println("------------receiveTicket----------------");
+        System.out.println(model.toString());
+        System.out.println("--------------receiveTicket--------------");
+        BookingProcess booking = new BookingProcess();
+        booking.writeRequestToRepo(model);
+        TicketWrapper ticket = new TicketWrapper(booking.getRepo());
+        return ticket.receiveTicket();
+    }
+
+    @RequestMapping(value = "/ticket",
+            method = RequestMethod.POST,
+            consumes = {"application/xml", "application/json"})
+    @ResponseBody
+    public ResponseEntity<?> receiveXmlOrJsonTicket(@RequestBody final String input) {
+        System.out.println("------------receiveXmlOrJsonTicket----------------");
+        System.out.println(input);
+        System.out.println("--------------receiveXmlOrJsonTicket--------------");
+        try {
+            Utils.sendPostRequest("http://localhost:8801/dfki/ticket/service/ticket", input, new String[]{"application/xml", "application/json"});
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(SpringExceptionHandlers.handleIoException(e), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
 }
