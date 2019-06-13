546a6f05b18a9eb5d5cbbb2ac508b1f7a8e6ae6d
DESKTOP-R3ROKK7\Kazimov <kazimovekber@gmail.com>
2019-06-13 14:14:06 +0200

    >>>>>>>Made lots of checkstyle-rules optimizations

--- dfki-ticket-service/src/main/java/com/dfki/services/dfki_ticket_service/services/TicketService.java
+++ dfki-ticket-service/src/main/java/com/dfki/services/dfki_ticket_service/services/TicketService.java
@@ -5,6 +5,7 @@ import com.dfki.services.dfki_ticket_service.models.Ticket;
 import com.dfki.services.dfki_ticket_service.repositories.TicketRepo;
 import org.eclipse.rdf4j.model.Model;
 import org.springframework.stereotype.Component;
+
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.util.Map;
@@ -12,17 +13,12 @@ import java.util.Map;
 @Component
 public class TicketService {
     private TicketRepo ticketRepo;
-    private final static String[] VDV_SERVICE_URIs = {"http://localhost:8802/vdv/ticket",
-            "http://192.168.99.100:8802/vdv/ticket"};
-    public final static String VDV_SERVICE_URI = VDV_SERVICE_URIs[0];
-//    private final static String vdv_ticket_service_url_localhost = "http://localhost:8802/vdv/ticket";
-//    private final static String vdv_ticket_service_url_docker = "http://192.168.99.100:8802/vdv/ticket";
 
     public TicketService() {
         ticketRepo = new TicketRepo();
     }
 
-    public Ticket save(String rdfInput) throws IOException {
+    public Ticket save(final String rdfInput) throws IOException {
         Model model = Utils.turtleToRDFConverter(rdfInput);
         Map<String, String> prefixes = Utils.parsePrefixes(rdfInput);
         if (ticketRepo.save(model)) {
@@ -33,32 +29,30 @@ public class TicketService {
         return null;
     }
 
-    public String toJson(Ticket ticket) {
+    public String toJson(final Ticket ticket) {
         return Utils.convertObjectToJson(ticket);
     }
 
-    public String toXml(Ticket ticket) {
+    public String toXml(final Ticket ticket) {
         return Utils.convertObjectToXML(ticket);
     }
 
-    public void postToVdvService(Ticket ticket) throws Exception {
+    public void postToVdvService(final Ticket ticket) throws Exception {
 
-        Utils.sendPostRequest(VDV_SERVICE_URI, toXml(ticket), new String[]{MediaType.APPLICATION_XML});
+        Utils.sendPostRequest(Utils.VDV_SERVICE_URI, toXml(ticket), new String[]{MediaType.APPLICATION_XML});
     }
 
-    public String xmlToRdf(String xmlString) throws Exception {
+    public String xmlToRdf(final String xmlString) throws Exception {
         String mappingFile = "xml_mapping.ttl";
         String fileName = "xml_text.xml";
         Utils.writeTextToFile(fileName, xmlString);
-        String result = Utils.mapToRDF(mappingFile);
-        return result;
+        return Utils.mapToRDF(mappingFile);
     }
 
-    public String jsonToRdf(String jsonString) throws Exception {
+    public String jsonToRdf(final String jsonString) throws Exception {
         String mappingFile = "json_mapping.ttl";
         String fileName = "json_text.json";
         Utils.writeTextToFile(fileName, jsonString);
-        String result = Utils.mapToRDF(mappingFile);
-        return result;
+        return Utils.mapToRDF(mappingFile);
     }
 }
