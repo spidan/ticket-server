2c651bcc1b68504ea120366d0a9a67b534840dac
DESKTOP-R3ROKK7\Kazimov <kazimovekber@gmail.com>
2019-06-17 12:12:23 +0200

    Some minor spotbugs errors are corrected in this commit.

--- smartticket/src/main/java/de/dfki/asr/smartticket/exceptions/CustomExceptionHandler.java
+++ smartticket/src/main/java/de/dfki/asr/smartticket/exceptions/CustomExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class CustomExceptionHandler {
     @ResponseBody
     @ExceptionHandler(value = {CustomException.class})
-    public ResponseEntity<Object> handleException(CustomException e) {
+    public ResponseEntity<Object> handleException(final CustomException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
