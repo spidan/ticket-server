d8f51fa7c63f139cf87e0bdf3eac1d6edacd0923
DESKTOP-R3ROKK7\Kazimov <kazimovekber@gmail.com>
2019-06-17 10:59:09 +0200

    In Ticket Controller, now exceptions are handled with CustomException and CustomExceptionHandler class.

--- dfki-ticket-service/src/main/java/com/dfki/services/dfki_ticket_service/controllers/TicketController.java
+++ dfki-ticket-service/src/main/java/com/dfki/services/dfki_ticket_service/controllers/TicketController.java
@@ -1,6 +1,7 @@
 package com.dfki.services.dfki_ticket_service.controllers;
 
 import com.dfki.services.dfki_ticket_service.Utils;
+import com.dfki.services.dfki_ticket_service.exceptions.CustomException;
 import com.dfki.services.dfki_ticket_service.models.Ticket;
 import com.dfki.services.dfki_ticket_service.services.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,30 +11,39 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 
-@RestController("TicketController")
+
+@RestController()
 public class TicketController {
     @Autowired
     private TicketService ticketService;
 
-    @PostMapping(value = "dfki/ticket/service/ticket", consumes = "text/turtle")
+    @PostMapping(value = "/ticket", consumes = "text/turtle")
     public ResponseEntity<?> saveTicketRdf(@RequestBody final String rdfInput) {
+        Ticket ticket = null;
         try {
-            Ticket ticket = ticketService.save(rdfInput);
+            ticket = ticketService.save(rdfInput);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new CustomException(Utils.INVALID_TURTLE_ERROR_MSG + "Additional info:"
+                    + e.getMessage());
+        }
 
-            String ticketJson = ticketService.toJson(ticket);
-
+        String ticketJson = ticketService.toJson(ticket);
+        try {
             ticketService.postToVdvService(ticket);
-
-            return new ResponseEntity<>(ticketJson, HttpStatus.OK);
-
         } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>("Process Failed", HttpStatus.NOT_ACCEPTABLE);
+            throw new CustomException(Utils.VDV_SERVICE_CONN_ERR_MSG + " Additional info:"
+                    + e.getMessage());
         }
+
+        return ResponseEntity.status(HttpStatus.OK).body(ticketJson);
+
     }
 
-    @PostMapping(value = "dfki/ticket/service/ticket", consumes = {"application/xml", "application/json"})
+    @PostMapping(value = "/ticket", consumes = {"application/xml", "application/json"})
     public ResponseEntity<?> saveTicket(@RequestBody final String input) {
         String result = "";
         try {
@@ -42,25 +52,26 @@ public class TicketController {
             } else if (Utils.isValidJson(input)) {
                 result = ticketService.jsonToRdf(input);
             } else {
-                return new ResponseEntity<>("The input is neither JSON nor XML!!!", HttpStatus.NOT_ACCEPTABLE);
+                throw new CustomException(Utils.JSON_XML_ERR_MSG);
             }
-        } catch (Exception e) {
+        } catch (Error | Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>("Mapping process failed. Most likely, mapping file is "
-                    + "not proper for the input.\n" + e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+            throw new CustomException(Utils.MAPPING_ERR_MSG + "Additional info:"
+                    + e.getMessage());
         }
         if (result.equals("")) {
-            return new ResponseEntity<>("Mapping process failed. Most likely, mapping file is "
-                    + "not proper for the input.\n", HttpStatus.NOT_ACCEPTABLE);
+            throw new CustomException(Utils.MAPPING_ERR_MSG);
         } else {
             try {
                 result = Utils.sendPostRequest(Utils.SMART_TICKET_URL, result,
                         new String[]{Utils.TURTLE_MEDIA_TYPE});
             } catch (Exception e) {
                 e.printStackTrace();
-                return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+
+                throw new CustomException(Utils.SMART_TICKET_CONN_ERR_MSG + "Additional info:"
+                        + e.getMessage());
             }
         }
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
