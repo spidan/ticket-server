1a6a8b6dab0735c950a96703b185578daf2ec8d7
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2019-08-23 11:33:46 +0200

    Whitespaces

--- dfki-ticket-service/src/main/java/com/dfki/services/dfki_ticket_service/DfkiTicketServiceApplication.java
+++ dfki-ticket-service/src/main/java/com/dfki/services/dfki_ticket_service/DfkiTicketServiceApplication.java
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
 public class DfkiTicketServiceApplication {
-
     public static void main(final String[] args) {
         SpringApplication.run(DfkiTicketServiceApplication.class, args);
         System.out.println("PROJECT is Running!!!");
     }
-
 }
