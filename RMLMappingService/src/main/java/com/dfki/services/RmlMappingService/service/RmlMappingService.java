a7d1a13cb2e4931873814890f507257aa0022207
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2020-01-22 15:26:28 +0100

    Removed ticket specific procedure from the mapping service class

--- RMLMappingService/src/main/java/com/dfki/services/RmlMappingService/service/RmlMappingService.java
+++ RMLMappingService/src/main/java/com/dfki/services/RmlMappingService/service/RmlMappingService.java
@@ -3,36 +3,15 @@ package com.dfki.services.RmlMappingService.service;
 import com.dfki.services.RmlMappingService.Utils;
 import com.dfki.services.RmlMappingService.exceptions.InvalidInputException;
 import com.dfki.services.RmlMappingService.exceptions.RmlMappingException;
-import com.dfki.services.RmlMappingService.exceptions.ServiceConnectionException;
 import com.dfki.services.RmlMappingService.models.Ticket;
-import com.dfki.services.RmlMappingService.repositories.TicketRepo;
-import org.eclipse.rdf4j.model.Model;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.core.MediaType;
-import java.io.IOException;
 
 @Component
 public class RmlMappingService {
-    private TicketRepo ticketRepo;
 
     public RmlMappingService() {
-        ticketRepo = new TicketRepo();
-    }
 
-    public Ticket save(final String rdfInput) {
-
-        Model model;
-        try {
-            model = Utils.turtleToRDFConverter(rdfInput);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new InvalidInputException("Tutrle", e.getMessage());
-        }
-//        Map<String, String> prefixes = Utils.parsePrefixes(rdfInput);
-        ticketRepo.save(model);
-        return Utils.getTicketFromDB(new Ticket(), ticketRepo);
-//            ticket.setPrefixes(prefixes);
     }
 
     public String toJson(final Ticket ticket) {
@@ -41,25 +20,6 @@ public class RmlMappingService {
 
     public String toXml(final Ticket ticket) {
         return Utils.convertObjectToXML(ticket);
-    }
-
-    public void postToVdvService(final Ticket ticket) {
-        try {
-            Utils.sendPostRequest(Utils.VDV_SERVICE_URI, toXml(ticket), new String[]{MediaType.APPLICATION_XML});
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new ServiceConnectionException("VdvTicket", e.getMessage());
-        }
-    }
-
-    public String postToSmartTicketService(final String result) {
-        try {
-            return Utils.sendPostRequest(Utils.SMART_TICKET_URL, result,
-                    new String[]{Utils.TURTLE_MEDIA_TYPE});
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new ServiceConnectionException("SmartTicket", e.getMessage());
-        }
     }
 
     public String xmlToRdf(final String xmlString) throws Exception {
