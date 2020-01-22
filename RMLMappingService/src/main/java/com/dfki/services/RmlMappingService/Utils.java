a110cbe08f539634713a12d3f42f5caf1c2faa6e
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2020-01-22 15:26:10 +0100

    Removed ticket specific functionality from Utils

--- RMLMappingService/src/main/java/com/dfki/services/RmlMappingService/Utils.java
+++ RMLMappingService/src/main/java/com/dfki/services/RmlMappingService/Utils.java
@@ -5,16 +5,8 @@ import be.ugent.rml.functions.FunctionLoader;
 import be.ugent.rml.records.RecordsFactory;
 import be.ugent.rml.store.QuadStore;
 import be.ugent.rml.store.RDF4JStore;
-import com.dfki.services.RmlMappingService.models.Ticket;
-import com.dfki.services.RmlMappingService.repositories.TicketRepo;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.eclipse.rdf4j.model.*;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.Rio;
@@ -44,11 +36,7 @@ public final class Utils {
 
     }
 
-    public static final String SMART_TICKET_URL = "http://localhost:8090/ticket";
     public static final String TURTLE_MEDIA_TYPE = "text/turtle";
-    private static final String[] VDV_SERVICE_URIS = {"http://localhost:8802/vdv/ticket",
-            "http://192.168.99.100:8802/vdv/ticket"};
-    public static final String VDV_SERVICE_URI = VDV_SERVICE_URIS[0];
 
     private static final String WORKING_DIRECTORY =
             new File(Utils.class.getResource("/application.properties").getFile()).getParentFile().getAbsolutePath();
@@ -56,75 +44,6 @@ public final class Utils {
     public static Model turtleToRDFConverter(final String input) throws IOException {
         InputStream inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
         return Rio.parse(inputStream, "", RDFFormat.TURTLE);
-    }
-
-    public static Ticket rdfToTicketConverter(final Model model) {
-        Ticket ticket = new Ticket();
-
-        for (Statement statement : model) {
-            System.out.println(statement);
-            IRI property = statement.getPredicate();
-            Value value = statement.getObject();
-            if (value instanceof Literal) {
-                Literal literal = (Literal) value;
-//                System.out.println("Data Type: " + literal.getDatatype());
-                if (property.getLocalName().equals("name")) {
-                    String name = literal.getLabel();
-                    ticket.setName(name);
-                } else if (property.getLocalName().equals("validFrom")) {
-                    String begin = literal.getLabel();
-                    ticket.setBegin(begin);
-//                    System.out.println("validfrom Date: " + date);
-                } else if (property.getLocalName().equals("validThrough")) {
-                    String end = literal.getLabel();
-                    ticket.setEnd(end);
-//                    System.out.println("validThrough Date: " + date);
-                }
-//
-//                System.out.println("Lexical value: '" + literal.getLabel() + "'");
-//            }
-            }
-        }
-        return ticket;
-
-    }
-
-    public static Ticket getTicketFromDB(final Ticket ticket, final TicketRepo ticketRepo) {
-        String name = ticketRepo.getObject("sm", "offer1", "gr", "name");
-        name = convertFromWithUrlToWithoutUrl("gr", name);
-        ticket.setName(name);
-
-        String begin = ticketRepo.getObject("sm", "offer1", "gr", "validFrom");
-        begin = convertFromWithUrlToWithoutUrl("gr", begin);
-        ticket.setBegin(begin);
-
-        String end = ticketRepo.getObject("sm", "offer1", "gr", "validThrough");
-        end = convertFromWithUrlToWithoutUrl("gr", end);
-        ticket.setEnd(end);
-
-        String includes = ticketRepo.getObject("sm", "offer1", "gr", "includes");
-        includes = convertFromWithUrlToWithoutUrl("gr", includes);
-        ticket.setIncludes(includes);
-        includes = includes.substring(includes.indexOf(":") + 1);
-
-        String accessedBus = ticketRepo.getObject("sm", includes, "tio", "accessTo");
-        accessedBus = convertFromWithUrlToWithoutUrl("tio", accessedBus);
-        ticket.setAccessedBus(accessedBus);
-        accessedBus = accessedBus.substring(accessedBus.indexOf(":") + 1);
-
-        String fromStation = ticketRepo.getObject("sm", accessedBus, "tio", "from");
-        fromStation = convertFromWithUrlToWithoutUrl("tio", fromStation);
-        ticket.setFromStation(fromStation);
-
-        String toStation = ticketRepo.getObject("sm", accessedBus, "tio", "to");
-        toStation = convertFromWithUrlToWithoutUrl("tio", toStation);
-        ticket.setToStation(toStation);
-
-        String type = ticketRepo.getType("sm", includes, "tio");
-        type = convertFromWithUrlToWithoutUrl("tio", type);
-        ticket.setType(type);
-
-        return ticket;
     }
 
     public static String convertFromWithUrlToWithoutUrl(final String prefix, final String value) {
@@ -157,23 +76,6 @@ public final class Utils {
         }
 
         return xmlResult;
-    }
-
-    public static String sendPostRequest(final String url, final String data,
-                                         final String[] contentTypes) throws Exception {
-        CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost(url);
-        StringEntity stringEntity = new StringEntity(data);
-        httpPost.setEntity(stringEntity);
-        for (String contentType : contentTypes) {
-            httpPost.setHeader("Accept", contentType);
-            httpPost.setHeader("Content-type", contentType);
-        }
-
-        CloseableHttpResponse closeableHttpResponse = closeableHttpClient.execute(httpPost);
-        String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), Charset.defaultCharset());
-        closeableHttpClient.close();
-        return responseString;
     }
 
     public static Map<String, String> parsePrefixes(final String rdfInput) {
