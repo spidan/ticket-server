436096deb70ff5bf70bb02b4e9f95ad9903bd4aa
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2020-01-22 14:36:16 +0100

    Removed endpoints that are only ticket relevant

--- RMLMappingService/src/main/java/com/dfki/services/RmlMappingService/controllers/TicketController.java
+++ RMLMappingService/src/main/java/com/dfki/services/RmlMappingService/controllers/TicketController.java
@@ -1,6 +1,5 @@
 package com.dfki.services.RmlMappingService.controllers;
 
-import com.dfki.services.RmlMappingService.models.Ticket;
 import com.dfki.services.RmlMappingService.services.TicketService;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -12,8 +11,6 @@ import org.eclipse.rdf4j.rio.RDFParser;
 import org.eclipse.rdf4j.rio.Rio;
 import org.eclipse.rdf4j.rio.helpers.StatementCollector;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,33 +22,14 @@ public class TicketController {
 	@Autowired
 	private TicketService ticketService;
 
-	@PostMapping(value = "/ticket", consumes = "text/turtle")
-	public ResponseEntity<?> saveTicketRdf(@RequestBody final String rdfInput) {
-		Ticket ticket = ticketService.save(rdfInput);
-		String ticketJson = ticketService.toJson(ticket);
-		ticketService.postToVdvService(ticket);
-		return ResponseEntity.status(HttpStatus.OK).body(ticketJson);
-	}
-
-	@PostMapping(value = "/ticket", consumes = {"application/xml", "application/json"})
-	public String convertTicket(@RequestBody final String input) throws IOException {
-		String result = ticketService.convertToRdf(input);
-		InputStream rdfStream = new ByteArrayInputStream(result.getBytes("utf-8"));
-		RDFParser parser = Rio.createParser(RDFFormat.TURTLE);
-		Model model = new LinkedHashModel();
-		parser.setRDFHandler(new StatementCollector(model));
-		parser.parse(rdfStream, SERVICE_URL);
-		return result;
-	}
-
 	@PostMapping(value = "/maptordf", consumes = {"application/xml", "application/json"})
-	public String mapToRdf(@RequestBody final String input) throws IOException {
+	public Model mapToRdf(@RequestBody final String input) throws IOException {
 		String result = ticketService.convertToRdf(input);
 		InputStream rdfStream = new ByteArrayInputStream(result.getBytes("utf-8"));
 		RDFParser parser = Rio.createParser(RDFFormat.TURTLE);
 		Model model = new LinkedHashModel();
 		parser.setRDFHandler(new StatementCollector(model));
 		parser.parse(rdfStream, SERVICE_URL);
-		return result;
+		return model;
 	}
 }
