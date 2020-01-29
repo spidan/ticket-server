f44dfb8007b721116fe867eabd1e0b817b8afa39
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2020-01-29 10:17:03 +0100

    Autoformat for more beauty of the mapping source

--- RMLMappingService/src/main/java/com/dfki/services/RmlMappingService/service/RmlMappingService.java
+++ RMLMappingService/src/main/java/com/dfki/services/RmlMappingService/service/RmlMappingService.java
@@ -4,55 +4,46 @@ import com.dfki.services.RmlMappingService.Utils;
 import com.dfki.services.RmlMappingService.exceptions.InvalidInputException;
 import com.dfki.services.RmlMappingService.exceptions.RmlMappingException;
 import com.dfki.services.RmlMappingService.models.Ticket;
+import com.taxonic.carml.engine.RmlMapper;
+import com.taxonic.carml.logical_source_resolver.JsonPathResolver;
+import com.taxonic.carml.model.TriplesMap;
+import com.taxonic.carml.util.RmlMappingLoader;
+import com.taxonic.carml.vocab.Rdf;
+import java.io.InputStream;
+import java.util.Set;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.rio.RDFFormat;
 import org.springframework.stereotype.Component;
-
 
 @Component
 public class RmlMappingService {
 
-    public RmlMappingService() {
+	public RmlMappingService() {
 
-    }
+	}
 
-    public String toJson(final Ticket ticket) {
-        return Utils.convertObjectToJson(ticket);
-    }
+	public String toJson(final Ticket ticket) {
+		return Utils.convertObjectToJson(ticket);
+	}
 
-    public String toXml(final Ticket ticket) {
-        return Utils.convertObjectToXML(ticket);
-    }
+	public String toXml(final Ticket ticket) {
+		return Utils.convertObjectToXML(ticket);
+	}
 
-    public String xmlToRdf(final String xmlString) throws Exception {
-        String mappingFile = "xml_mapping.ttl";
-        String fileName = "xml_text.xml";
-        Utils.writeTextToFile(fileName, xmlString);
-        return Utils.mapToRDF(mappingFile);
-    }
+	public String xmlToRdf(final String xmlString) throws Exception {
+		String mappingFile = "xml_mapping.ttl";
+		String fileName = "xml_text.xml";
+		Utils.writeTextToFile(fileName, xmlString);
+		return Utils.mapToRDF(mappingFile);
+	}
 
-    public String jsonToRdf(final String jsonString) throws Exception {
-        String mappingFile = "transport_mapping.ttl";
-        String fileName = "json_text.json";
-        Utils.writeTextToFile(fileName, jsonString);
-        return Utils.mapToRDF(mappingFile);
-    }
+	public String jsonToRdf(final String jsonString) throws Exception {
+		String mappingFile = "transport_mapping.ttl";
+		String fileName = "json_text.json";
+		Utils.writeTextToFile(fileName, jsonString);
+		return Utils.mapToRDF(mappingFile);
+	}
 
-    public String convertToRdf(final String input) {
-        try {
-            if (Utils.isValidXml(input)) {
-                return xmlToRdf(input);
-            } else if (Utils.isValidJson(input)) {
-                return jsonToRdf(input);
-            } else {
-                throw new InvalidInputException("XML or JSON", "");
-            }
-        } catch (InvalidInputException e) {
-            throw e;
-        } catch (Error | Exception e) {
-            e.printStackTrace();
-            throw new RmlMappingException(e.getMessage());
-        }
-
-    }
 	public Model mapJSONWithCarml(InputStream mappingFileStream, InputStream resourceStream) {
 		Set<TriplesMap> mapping
 			= RmlMappingLoader
@@ -68,4 +59,21 @@ public class RmlMappingService {
 		return result;
 	}
 
+	public String convertToRdf(final String input) {
+		try {
+			if (Utils.isValidXml(input)) {
+				return xmlToRdf(input);
+			} else if (Utils.isValidJson(input)) {
+				return jsonToRdf(input);
+			} else {
+				throw new InvalidInputException("XML or JSON", "");
+			}
+		} catch (InvalidInputException e) {
+			throw e;
+		} catch (Error | Exception e) {
+			e.printStackTrace();
+			throw new RmlMappingException(e.getMessage());
+		}
+
+	}
 }
