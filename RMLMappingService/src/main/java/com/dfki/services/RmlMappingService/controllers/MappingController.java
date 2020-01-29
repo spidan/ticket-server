928db70210828d9ffd210bff1da3bc75642f00fa
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2020-01-29 13:40:14 +0100

    Extracted method for ttl output

--- RMLMappingService/src/main/java/com/dfki/services/RmlMappingService/controllers/MappingController.java
+++ RMLMappingService/src/main/java/com/dfki/services/RmlMappingService/controllers/MappingController.java
@@ -7,8 +7,10 @@ import java.io.OutputStream;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Statement;
 import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFHandlerException;
 import org.eclipse.rdf4j.rio.RDFWriter;
 import org.eclipse.rdf4j.rio.Rio;
+import org.eclipse.rdf4j.rio.UnsupportedRDFormatException;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -30,18 +32,23 @@ public class MappingController {
 	public ResponseEntity<?> mapToRdf(@RequestBody final String input) throws IOException {
 		try {
 			Model result = mappingService.jsonToRdf(input);
-			OutputStream turtleOutput = new ByteArrayOutputStream();
-			RDFWriter rdfWriter = Rio.createWriter(RDFFormat.TURTLE,
-				turtleOutput);
-			rdfWriter.startRDF();
-			for (Statement st: result) {
-				rdfWriter.handleStatement(st);
-			}
-			rdfWriter.endRDF();
+			OutputStream turtleOutput = modelToTtl(result);
 			return new ResponseEntity<>(turtleOutput.toString(), HttpStatus.OK);
 		} catch (Exception ex) {
 			LOG.error("Error processing input: " + ex.toString());
 			return new ResponseEntity<>("Invalid input: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
 		}
+	}
+
+	private OutputStream modelToTtl(final Model result) throws UnsupportedRDFormatException, RDFHandlerException {
+		OutputStream turtleOutput = new ByteArrayOutputStream();
+		RDFWriter rdfWriter = Rio.createWriter(RDFFormat.TURTLE,
+			turtleOutput);
+		rdfWriter.startRDF();
+		for (Statement st: result) {
+			rdfWriter.handleStatement(st);
+		}
+		rdfWriter.endRDF();
+		return turtleOutput;
 	}
 }
