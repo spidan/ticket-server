577ba15776deb3f43598a70a0ad14a890fb80500
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2020-01-30 11:05:31 +0100

    GET endpoint for receiving a mapping file by filename

--- RMLMappingService/src/main/java/com/dfki/services/RmlMappingService/controllers/MappingFileProvider.java
+++ RMLMappingService/src/main/java/com/dfki/services/RmlMappingService/controllers/MappingFileProvider.java
@@ -1,5 +1,17 @@
 package com.dfki.services.RmlMappingService.controllers;
 
+import io.micrometer.core.instrument.util.IOUtils;
+import java.io.InputStream;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
 public class MappingFileProvider {
 
+	@GetMapping(value = "/mappingfile", produces = {"text/turtle"})
+	public String getMappingFile(@RequestParam final String filename) {
+		InputStream mappingStream = ClassLoader.getSystemResourceAsStream(filename);
+		return IOUtils.toString(mappingStream);
+	}
 }
