7e4a2cd72847dff5c040c8f6d13bfdd07299da36
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2020-02-04 10:30:31 +0100

    Changed sendPostRequest to use try with resources

--- smartticket/src/main/java/de/dfki/asr/smartticket/service/Utils.java
+++ smartticket/src/main/java/de/dfki/asr/smartticket/service/Utils.java
@@ -8,6 +8,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.nio.charset.Charset;
+import org.graalvm.compiler.core.common.SuppressFBWarnings;
 
 public final class Utils {
     private Utils() {
@@ -16,23 +17,23 @@ public final class Utils {
 
     public static final String DFKI_TICKET_SERVICE_URL = "http://localhost:8801/maptordf";
 
+    @SuppressFBWarnings(value = "RCN_REDUNDANT_NULLCHECK_WOULD_HAVE_BEEN_A_NPE",
+		justification = "Unfixable external code")
     public static String sendPostRequest(final String url, final String data,
                                          final String[] contentTypes) throws Exception {
-        CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost(url);
-        StringEntity stringEntity = new StringEntity(data);
-        httpPost.setEntity(stringEntity);
-        for (String contentType : contentTypes) {
-            httpPost.setHeader("Accept", contentType);
-            httpPost.setHeader("Content-type", contentType);
-        }
-
-        CloseableHttpResponse closeableHttpResponse = closeableHttpClient.execute(httpPost);
-        String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), Charset.defaultCharset());
-        closeableHttpClient.close();
-        closeableHttpResponse.close();
-
-
+	String responseString;
+	try (CloseableHttpClient closeableHttpClient = HttpClients.createDefault()) {
+	    HttpPost httpPost = new HttpPost(url);
+	    StringEntity stringEntity = new StringEntity(data);
+	    httpPost.setEntity(stringEntity);
+	    for (String contentType : contentTypes) {
+		httpPost.setHeader("Accept", contentType);
+		httpPost.setHeader("Content-type", contentType);
+	    }
+	    try (CloseableHttpResponse closeableHttpResponse = closeableHttpClient.execute(httpPost)) {
+		responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), Charset.defaultCharset());
+	    }
+	}
         return responseString;
     }
 }
