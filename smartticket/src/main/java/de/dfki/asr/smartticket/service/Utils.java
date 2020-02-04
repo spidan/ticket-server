248218a5d96b7412adf7ba57b46e3037f36b9502
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2020-02-04 10:30:57 +0100

    Autoformat of Utils

--- smartticket/src/main/java/de/dfki/asr/smartticket/service/Utils.java
+++ smartticket/src/main/java/de/dfki/asr/smartticket/service/Utils.java
@@ -6,11 +6,11 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-
 import java.nio.charset.Charset;
 import org.graalvm.compiler.core.common.SuppressFBWarnings;
 
 public final class Utils {
+
     private Utils() {
 
     }
@@ -18,11 +18,11 @@ public final class Utils {
     public static final String DFKI_TICKET_SERVICE_URL = "http://localhost:8801/maptordf";
 
     @SuppressFBWarnings(value = "RCN_REDUNDANT_NULLCHECK_WOULD_HAVE_BEEN_A_NPE",
-		justification = "Unfixable external code")
+	    justification = "Unfixable external code")
     public static String sendPostRequest(final String url, final String data,
-                                         final String[] contentTypes) throws Exception {
+	    final String[] contentTypes) throws Exception {
 	String responseString;
-	try (CloseableHttpClient closeableHttpClient = HttpClients.createDefault()) {
+	try ( CloseableHttpClient closeableHttpClient = HttpClients.createDefault()) {
 	    HttpPost httpPost = new HttpPost(url);
 	    StringEntity stringEntity = new StringEntity(data);
 	    httpPost.setEntity(stringEntity);
@@ -30,10 +30,10 @@ public final class Utils {
 		httpPost.setHeader("Accept", contentType);
 		httpPost.setHeader("Content-type", contentType);
 	    }
-	    try (CloseableHttpResponse closeableHttpResponse = closeableHttpClient.execute(httpPost)) {
+	    try ( CloseableHttpResponse closeableHttpResponse = closeableHttpClient.execute(httpPost)) {
 		responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), Charset.defaultCharset());
 	    }
 	}
-        return responseString;
+	return responseString;
     }
 }
