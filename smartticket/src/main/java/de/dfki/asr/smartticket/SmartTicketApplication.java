5e6f84050d9caef477c9460d0b3e37d3de1b6841
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2020-02-04 12:26:22 +0100

    Create singleton bean for simple map registry

--- smartticket/src/main/java/de/dfki/asr/smartticket/SmartTicketApplication.java
+++ smartticket/src/main/java/de/dfki/asr/smartticket/SmartTicketApplication.java
@@ -1,7 +1,10 @@
 package de.dfki.asr.smartticket;
 
+import de.dfki.asr.smartticket.service.TemplateRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
@@ -9,5 +12,11 @@ public class SmartTicketApplication {
 
     public static void main(final String[] args) {
 	SpringApplication.run(SmartTicketApplication.class, args);
+    }
+
+    @Bean
+    @Scope("singleton")
+    public TemplateRegistry templateRegistry() {
+	return new TemplateRegistry();
     }
 }
