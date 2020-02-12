cd473c49c80c1e330d00d3b5207653c6385f28f3
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2020-02-12 10:50:12 +0100

    Added endpoint to post turtle to the service registry

--- smartticket/src/main/java/de/dfki/asr/smartticket/Rest/RegistryEndpoint.java
+++ smartticket/src/main/java/de/dfki/asr/smartticket/Rest/RegistryEndpoint.java
@@ -4,11 +4,13 @@ import de.dfki.asr.smartticket.data.RDFServiceRegistry;
 import de.dfki.asr.smartticket.service.ServiceRegistry;
 import java.net.URI;
 import de.dfki.asr.smartticket.service.TemplateRegistry;
+import org.eclipse.rdf4j.model.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,6 +38,14 @@ public class RegistryEndpoint {
 	    return new ResponseEntity<>("Could not register mapping: " + ex.getMessage(),
 		    HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+	return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/serviceTemplate",
+	    method = RequestMethod.POST,
+	    consumes = "text/turtle")
+    public ResponseEntity<?> registerRDFTemplate(@RequestBody final Model serviceModel) {
+	serviceRegistry.write(serviceModel);
 	return new ResponseEntity<>(HttpStatus.OK);
     }
 
