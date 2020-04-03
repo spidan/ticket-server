82c2f647275e3d8e56775581afd7e2b11c940091
Daniel Spieldenner <DanielSpieldenner@gmail.com>
2020-04-03 14:58:39 +0200

    Build request JSON from template making use of triple store

--- smartticket/src/main/java/de/dfki/asr/smartticket/service/JsonTemplate.java
+++ smartticket/src/main/java/de/dfki/asr/smartticket/service/JsonTemplate.java
@@ -1,5 +1,6 @@
 package de.dfki.asr.smartticket.service;
 
+import de.dfki.asr.smartticket.data.InMemoryRepo;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,39 +16,37 @@ public class JsonTemplate {
 	private JSONObject templateAsJson;
 	private JSONObject resultJson;
 
-	public void doStuffWithJSON(final String schemaString) {
-		templateAsJson = new JSONObject(schemaString);
 	public JsonTemplate(final String template) {
 		this.templateAsString = template;
 	}
 
+	public JSONObject generateOutputRequest(final InMemoryRepo repo) {
+		templateAsJson = new JSONObject(templateAsString);
 		JSONObject properties = templateAsJson.getJSONObject("properties");
-		JSONObject result = generateJsonFromProperties(properties);
+		JSONObject result = generateJsonFromProperties(properties, repo);
 		LOG.info(result.toString());
-		templateAsJson.keys().forEachRemaining(key -> {
-			if ("accessor".equals(key)) {
-				String queryString = templateAsJson.get(key).toString();
-				LOG.info(queryString);
-			}
-			LOG.info(key);
-		});
+		return resultJson;
 	}
 
-	private JSONObject generateJsonFromProperties(final JSONObject properties) {
+	private JSONObject generateJsonFromProperties(final JSONObject properties,
+							final InMemoryRepo repo) {
 		resultJson = new JSONObject();
 		properties.keys().forEachRemaining(key -> {
 			JSONObject propertyObject = properties.getJSONObject(key);
-			appendPropertyToResult(key, propertyObject);
+			appendPropertyToResult(key, propertyObject, repo);
 			LOG.info(key + ": " + propertyObject.toString());
 		});
 		return resultJson;
 	}
 
-	private void appendPropertyToResult(final String key, final JSONObject property) {
+	private void appendPropertyToResult(final String key,
+										final JSONObject property,
+										final InMemoryRepo repo) {
 		if (!property.has(ACCESSOR)) {
-			resultJson.append(key, property.get(DEFAULT));
+			resultJson.put(key, property.get(DEFAULT));
+		} else {
+			resultJson.put(key, replaceTemplatedValue(repo, property.getString(ACCESSOR)));
 		}
-		resultJson.append(key, property.get(ACCESSOR));
 	}
 
     private String replaceTemplatedValue(final InMemoryRepo repo, final String value) {
